646060a221e684e601db3e1155810bcbd57c23a8
omirbeck <ommirtull@gmail.com>
2020-08-25 01:40:09 +0600

    feat: add search for github name

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 //import {Route} from 'react-router-dom';
 import './App.css';
 
 import Sidebar from './components/Sidebar';
 
 function App() {
+  const [avatar, setAvatar] = useState('');
+  const [userInput, setUserInput] = useState('');
 
-  React.useEffect(() => {
-    fetch('https://api.github.com/users/example')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
+  // React.useEffect(() => {
+  //   fetch('https://api.github.com/users/example')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, [])
 
   const strongPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
 
@@ -20,22 +22,37 @@ function App() {
     };
   };
 
+  const handleLogin = (event) => {
+    setUserInput(event.target.value)
+  }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${userInput}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setData(data);
+      })
+  }
+
+  const setData = ({avatar_url}) => {
+    setAvatar(avatar_url);
+  }
 
   return (
     <div className="App">
       <div className="navbar">Github</div>
       <div className="login">
-        <form>
+        <form onSubmit={handleSubmit}> 
           <label>Login</label>
-          <input type="text" placeholder="Login"></input>
+          <input type="text" placeholder="Login" onChange={handleLogin}></input>
           <label>Password</label>
           <input type="password" placeholder="Password" onChange={analyze}></input>
           <input type="submit" value="Submit"></input>
         </form>
       </div>
-
-      <Sidebar />
+      <img src={avatar}></img>
     </div>
   );
 }
